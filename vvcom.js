a83fe6bb1252b22e1a81f092e58ac1d5c653d0ab
newnowave <gdrapps@hotmail.com>
2015-09-20 16:42:57 -0700

    "Added stylesheet and fixing bug of button do not doing anything"

--- vvcom.js
+++ vvcom.js
@@ -1,18 +1,18 @@
-function random_reason(reason_array){
-  var random_reason = reason_array[Math.floor(Math.random()*reason_array.length)]
-  return random_reason
-}
-
 var reason_array = [
   "Sí",
   "Ya sabes que sí, para que preguntas",
   "Hoy no"
 ];
 
-var reason = random_reason(reason_array);
-var reason_node = document.createElement("P");
-var reason_text = document.createTextNode(reason);
+function random_reason(reason_array){
+  var random_reason = reason_array[Math.floor(Math.random()*reason_array.length)]
+  return random_reason
+};
 
-reason_node.appendChild(reason_text);
-
-document.body.appendChild(reason_node)
+var show_reason = function(){
+  reason_from_array = random_reason(reason_array);
+  reason_text = document.createTextNode(reason_from_array);
+  reason_node = document.createElement("P");
+  reason_node.appendChild(reason_text);
+  document.body.appendChild(reason_node);
+};
